fc06ecf9d91262aa28dd88b1f3f84fa4eb327fe5
unknown <colaresedoardo@gmail.com>
2022-02-26 11:52:25 -0300

    A base dir do python

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -126,12 +126,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
 
 MEDIA_ROOT='imagens'
 MEDIA_URL ='/media/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
